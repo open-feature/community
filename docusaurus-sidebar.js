d3483f79a60f9338487693725482bdd646d95fb4
Michael Beemer <beeme1mr@users.noreply.github.com>
2024-03-29 04:33:22 -0400

    feat: add presentations page (#357)
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- docusaurus-sidebar.js
+++ docusaurus-sidebar.js
@@ -4,55 +4,59 @@
 const sidebarCommunity = {
   community: [
     {
-      type: 'doc',
-      id: 'README',
-      label: 'Overview',
+      type: "doc",
+      id: "README",
+      label: "Overview",
     },
 
     {
-      type: 'doc',
-      id: 'mission-vision',
+      type: "doc",
+      id: "mission-vision",
     },
     {
-      type: 'doc',
-      id: 'CONTRIBUTOR_LADDER',
+      type: "doc",
+      id: "CONTRIBUTOR_LADDER",
     },
     {
-      type: 'doc',
-      id: 'community-members',
-      label: 'Members',
+      type: "doc",
+      id: "community-members",
+      label: "Members",
     },
     {
-      type: 'category',
-      label: 'Charters',
+      type: "category",
+      label: "Charters",
       collapsible: false,
       items: [
         {
-          type: 'doc',
-          id: 'governance-charter',
+          type: "doc",
+          id: "governance-charter",
         },
         {
-          type: 'doc',
-          id: 'tech-committee-charter',
+          type: "doc",
+          id: "tech-committee-charter",
         },
       ],
     },
     {
-      type: 'doc',
-      id: 'interested-parties',
+      type: "doc",
+      id: "interested-parties",
     },
     {
-      type: 'doc',
-      id: 'ADOPTERS',
-      label: 'Adopters',
+      type: "doc",
+      id: "ADOPTERS",
+      label: "Adopters",
     },
     {
-      type: 'doc',
-      id: 'branding-guidelines',
+      type: "doc",
+      id: "presentations",
     },
     {
-      type: 'doc',
-      id: 'SECURITY',
+      type: "doc",
+      id: "branding-guidelines",
+    },
+    {
+      type: "doc",
+      id: "SECURITY",
     },
   ],
 };
